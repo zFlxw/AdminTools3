6dff12238e3254a320fc0a97359f6c35db066d36
zFlxw <lxcalflxw@gmail.com>
2020-02-21 13:43:38 +0100

    % Fixed Motd Bug

--- src/de/flxw/admintools/listener/ServerListPingEvent.java
+++ src/de/flxw/admintools/listener/ServerListPingEvent.java
@@ -18,21 +18,18 @@ public class ServerListPingEvent implements Listener
     {
         File file = FileManager.getMaintenanceFile();
         FileConfiguration cfg;
-        File configfile = FileManager.getConfigFile();
-        FileConfiguration configcfg;
 
         if(AdminTools.getInstance().Modt)
         {
             cfg = YamlConfiguration.loadConfiguration(file);
-            configcfg = YamlConfiguration.loadConfiguration(configfile);
-//            if(!cfg.getBoolean("maintenance") && AdminTools.getInstance().ActivateMaintenance)
-//            {
+            if(cfg.getBoolean("maintenance"))
+            {
                 evt.setMotd(AdminTools.getInstance().MaintenanceMotdLine1 + "\n" + AdminTools.getInstance().MaintenanceMotdLine2);
-//            }
-//            else
-//            {
-//                evt.setMotd(AdminTools.getInstance().ModtLine1 + "\n" + AdminTools.getInstance().ModtLine2);
-//            }
+            }
+            else
+            {
+                evt.setMotd(AdminTools.getInstance().ModtLine1 + "\n" + AdminTools.getInstance().ModtLine2);
+            }
         }
         evt.setMaxPlayers(AdminTools.getInstance().MaxPlayers);
     }
